e68469c642e5da7942bd7d548b0a78c7cc5a1d14
ippachi <ippachi1018@gmail.com>
2022-09-28 21:44:36 +0900

    fix(front): dry lvh.me:3000

--- frontend/module/workspace-id.ts
+++ frontend/module/workspace-id.ts
@@ -1,15 +1,20 @@
 /* istanbul ignore file */
 import { createContext, useContext, useEffect, useState } from "react";
 
+const hostname = process.env.NEXT_PUBLIC_HOSTNAME as string;
 const workspaceIdContext = createContext("");
 
 export const WorkspaceIdProvider = workspaceIdContext.Provider;
+
+// https://developer.mozilla.org/ja/docs/Web/JavaScript/Guide/Regular_Expressions#%E3%82%A8%E3%82%B9%E3%82%B1%E3%83%BC%E3%83%97
+const escapeRegExp = (string: string): string => string.replace(/[.*+?^${}()|[\]\\]/g, "\\$&");
 
 export const useWorkspaceIdOrNull = (): string | null => {
   const [workspaceId, setWorkspaceId] = useState<string | null>(null);
 
   useEffect(() => {
-    setWorkspaceId(window.location.host.replace(/\.?lvh\.me:3000$/, ""));
+    const regexp = new RegExp(`\\.?${escapeRegExp(hostname)}$`);
+    setWorkspaceId(window.location.host.replace(regexp, ""));
   }, []);
 
   return workspaceId;
