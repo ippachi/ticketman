7bf53f2b7e3c25528edc2ae6c8f49684b4eaecfa
ippachi <ippachi1018@gmail.com>
2022-09-28 23:27:16 +0900

    refactor: dry test

--- frontend/__tests__/pages/dashboard.test.tsx
+++ frontend/__tests__/pages/dashboard.test.tsx
@@ -1,18 +1,11 @@
 import Dashboard from "@/pages/dashboard";
-import { render, screen } from "@testing-library/react";
+import { render } from "@testing-library/react";
 
-const useWorkspaceMock = jest.fn();
-
-jest.mock("@/module/api", () => ({
-  useWorkspace: (workspaceId: string) => useWorkspaceMock(workspaceId),
-}));
-
+jest.mock("@/module/api");
 jest.mock("@/module/workspace-id");
 
 describe("Show", () => {
   test("render", () => {
-    useWorkspaceMock.mockImplementation(() => ({ data: { name: "workspace 1" } }));
     render(<Dashboard />);
-    expect(useWorkspaceMock).toHaveBeenCalledWith("test");
   });
 });
