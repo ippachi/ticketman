1ad39e3bcb84b7c70d027334ec2daa74e341a421
ippachi <ippachi1018@gmail.com>
2022-11-27 21:22:51 +0900

    refactor: Remove dead file

--- backend/lib/ticketman/gateway/organization_query.rb
+++ backend/lib/ticketman/gateway/organization_query.rb
@@ -4,8 +4,6 @@
 module Ticketman
   module Gateway
     class OrganizationQuery < Query
-      include Web::OrganizationQuery
-
       sig { params(organizations: Organizations).void }
       def initialize(organizations)
         super
