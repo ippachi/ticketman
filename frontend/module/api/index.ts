2e2ca5199c11fdef261a1408b9297570cd6f06ae
ippachi <ippachi1018@gmail.com>
2022-12-11 16:25:47 +0900

    refactor: Move mutation function code to common function.

--- frontend/module/api/index.ts
+++ frontend/module/api/index.ts
@@ -1,13 +1,9 @@
 import { useMutation, useQuery } from "@tanstack/react-query";
 import { useCallback } from "react";
-import {
-  CreateOrganizationMutationVariables,
-  GenerateAuthUrlMutationVariables,
-  Organization,
-  SigninMutationVariables,
-} from "src/gql/graphql";
+import { CreateOrganizationMutationVariables, Organization, SigninMutationVariables } from "src/gql/graphql";
 import { graphql } from "../../src/gql";
 import { GraphQLClient } from "graphql-request";
+import { TypedDocumentNode } from "@graphql-typed-document-node/core";
 
 export const client = new GraphQLClient("http://localhost:2300/graphql", { credentials: "include" });
 
@@ -51,6 +47,10 @@ const generateAuthUrlDocument = graphql(`
 
 type Errors = { message: string; extensions: { code: string } }[];
 
+type UseMutationResult<Variables, Data> = {
+  mutate: (variables: Variables, hooks: { onSuccess: (data: Data) => void; onError: (errors: Errors) => void }) => void;
+};
+
 export const useOrganization = (
   id: string,
   options: { useErrorBoundary: boolean } = { useErrorBoundary: false }
@@ -63,69 +63,40 @@ export const useOrganization = (
   return { data: data?.organization };
 };
 
-export const useCreateOrganizationMutation = (): {
-  mutate: (
-    variables: CreateOrganizationMutationVariables,
-    hooks: { onSuccess: (data: Organization) => void; onError: (errors: Errors) => void }
-  ) => void;
-} => {
-  const { mutate } = useMutation({
-    mutationFn: (variables: CreateOrganizationMutationVariables) =>
-      client.request(createOrganizationDocument, variables),
-  });
-  return {
-    mutate: useCallback(
-      (variables, hooks) =>
-        mutate(variables, {
-          onSuccess: (data) => hooks.onSuccess(data.createOrganization?.organization as Organization),
-          // eslint-disable-next-line @typescript-eslint/no-explicit-any, @typescript-eslint/no-unsafe-member-access
-          onError: (error: any) => hooks.onError(error.response?.errors as Errors),
-        }),
-      [mutate]
-    ),
-  };
+export const useCreateOrganizationMutation = (): UseMutationResult<
+  CreateOrganizationMutationVariables,
+  Organization
+> => {
+  return useBaseMutation(createOrganizationDocument, (data) => data.createOrganization?.organization as Organization);
 };
 
-export const useSigninMutation = (): {
-  mutate: (
-    variables: SigninMutationVariables,
-    hooks: { onSuccess: (data: null) => void; onError: (errors: Errors) => void }
-  ) => void;
-} => {
-  const { mutate } = useMutation({
-    mutationFn: (variables: SigninMutationVariables) => client.request(signinDocument, variables),
-  });
-  return {
-    mutate: useCallback(
-      (variables, hooks) =>
-        mutate(variables, {
-          onSuccess: (data) => hooks.onSuccess(data.signin?.user as null),
-          // eslint-disable-next-line @typescript-eslint/no-explicit-any, @typescript-eslint/no-unsafe-member-access
-          onError: (error: any) => hooks.onError(error.response?.errors as Errors),
-        }),
-      [mutate]
-    ),
-  };
+export const useSigninMutation = (): UseMutationResult<SigninMutationVariables, null> => {
+  return useBaseMutation(signinDocument, (data) => data.signin?.user as null);
 };
 
-export const useGenerateAuthUrlMutation = (): {
-  mutate: (
-    variables: Record<string, never>,
-    hooks: { onSuccess: (data: string) => void; onError: (errors: Errors) => void }
-  ) => void;
+export const useGenerateAuthUrlMutation = (): UseMutationResult<Record<string, never>, string> => {
+  return useBaseMutation(generateAuthUrlDocument, (data) => data.generateAuthUrl?.url as string);
+};
+
+const useBaseMutation = <T, V, E>(
+  document: TypedDocumentNode<T, V>,
+  onSuccessData: (data: T) => E
+): {
+  mutate: (variables: V, hooks: { onSuccess: (data: E) => void; onError: (errors: Errors) => void }) => void;
 } => {
   const { mutate } = useMutation({
-    mutationFn: (variables: GenerateAuthUrlMutationVariables) => client.request(generateAuthUrlDocument, variables),
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    mutationFn: (variables: V) => client.request(document, variables as Record<any, never>),
   });
   return {
     mutate: useCallback(
       (variables, hooks) =>
         mutate(variables, {
-          onSuccess: (data) => hooks.onSuccess(data.generateAuthUrl?.url as string),
+          onSuccess: (data) => hooks.onSuccess(onSuccessData(data)),
           // eslint-disable-next-line @typescript-eslint/no-explicit-any, @typescript-eslint/no-unsafe-member-access
           onError: (error: any) => hooks.onError(error.response?.errors as Errors),
         }),
-      [mutate]
+      [mutate, onSuccessData]
     ),
   };
 };
